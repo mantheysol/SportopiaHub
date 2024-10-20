fae3a4086b3e787e89e933f12c56226700979d93
mantheysol <prestenmanthey@yahoo.com>
2024-10-19 18:54:52 -0700

    Organize project structure and save changes

---
+++ sportopiahub-frontend/src/pages/PredictionPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PredictionPage = () => {
+  return (
+    <div>
+      <h1>Prediction Page</h1>
+      <p>Select upcoming matches and make your predictions here.</p>
+    </div>
+  );
+};
+
+export default PredictionPage;
